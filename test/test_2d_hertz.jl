bc1d478e54d658834e8fe64fee55c0b16cb761e0
Jukka Aho <ahojukka5@gmail.com>
2018-05-18 10:27:52 +0300

    fix license header

--- test/test_2d_hertz.jl
+++ test/test_2d_hertz.jl
@@ -1,5 +1,5 @@
 # This file is a part of JuliaFEM.
-# License is MIT: see https://github.com/JuliaFEM/MortarContact2D.jl/blob/master/LICENSE
+# License is MIT: see https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/LICENSE.md
 
 using JuliaFEM
 using JuliaFEM.Preprocess
