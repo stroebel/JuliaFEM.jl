987c5dd1cc3e8b70cdda1aba57f0b26c77d363fe
autodocs <autodocs>
2018-08-20 12:00:45 +0000

    build based on 1357e7e

--- latest/search_index.js
+++ latest/search_index.js
@@ -89,6 +89,46 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "examples/linear_static.html#",
+    "page": "JuliaFEM Linear Static Example",
+    "title": "JuliaFEM Linear Static Example",
+    "category": "page",
+    "text": "EditURL = \"https://github.com/JuliaFEM/JuliaFEM.jl/blob/master/examples/linear_static.jl\""
+},
+
+{
+    "location": "examples/linear_static.html#JuliaFEM-Linear-Static-Example-1",
+    "page": "JuliaFEM Linear Static Example",
+    "title": "JuliaFEM Linear Static Example",
+    "category": "section",
+    "text": "(Image: )"
+},
+
+{
+    "location": "examples/linear_static.html#Preprocessing-1",
+    "page": "JuliaFEM Linear Static Example",
+    "title": "Preprocessing",
+    "category": "section",
+    "text": "using JuliaFEM\nusing JuliaFEM.Preprocess\nusing JuliaFEM.Postprocess\nadd_elements! = JuliaFEM.add_elements!First we will read in the mesh. Geometry and mesh are greated with FreeCAD, where med format is selected for exporting. Mesh file consist also edge and surface mesh, which we will need to neglect later.datadir = Pkg.dir(\"JuliaFEM\", \"examples\", \"linear_static\")\nmeshfile = joinpath(datadir, \"JuliaFEMSMP18.med\")\nmesh = aster_read_mesh(meshfile)Next we will create the model and define Elasticity. Also elements are added to the model.model = Problem(Elasticity, \"OTHER\", 3)\nmodel_elements = create_elements(mesh, \"OTHER\")Elements need material properties and they are defined nextupdate!(model_elements, \"youngs modulus\", 208.0E3)\nupdate!(model_elements, \"poissons ratio\", 0.30)\nupdate!(model_elements, \"density\", 7.80E-9)\nadd_elements!(model, model_elements)We can ignore Seg3 and Tri6 elements using filter with a special function returning true if element is something else than Seg3 or Tri6:function is_not_Seg3_or_Tri6(element)\n    return !isa(element, Union{Element{Seg3}, Element{Tri6}})\nend\n\nfilter!(is_not_Seg3_or_Tri6, model.elements)The whole idea of the JuliaFEM input is to be a normal Julia script, where the user can freely define any functions needed to perform the task. Here we define a function, which finds nodes on the given plane yz, xz or xy from the given height.function add_nodes_at_certain_plane_to_node_set!(mesh, name, vector_id, distance, radius=6.0)\n    for (node, coords) in mesh.nodes\n        if isapprox(coords[vector_id], distance, atol=radius)\n            add_node_to_node_set!(mesh, name, node)\n        end\n    end\n    return nothing\nendWe will find nodes from the xz-plane going through point (0,50,0) or actually we previously defined the radius to be 6.0, which means (0,[44,56],0). In other words we will select each node, which second coordinate value is between 44 and 56. This function will edit mesh object and add node set called :mid_fixed to it.add_nodes_at_certain_plane_to_node_set!(mesh,:mid_fixed,2,50.0)We need to somehow handle the i\'s dot. I looked the rough coordinates of the dot in FreeCAD and now we can search three closest nodes to these coordinates. Those will be added to the same set :mid_fixed.ipoint = find_nearest_nodes(mesh, [165.0, 88.0, 10],3)\nfor poi in ipoint\n    add_node_to_node_set!(mesh, :mid_fixed, poi)\nendThe fixed boundary conditions are defined next.fixed = Problem(Dirichlet, \"fixed\", 3, \"displacement\")\nfixed_elements = create_nodal_elements(mesh, \"mid_fixed\")\nadd_elements!(fixed, fixed_elements)\nupdate!(fixed_elements, \"displacement 1\", 0.0)\nupdate!(fixed_elements, \"displacement 2\", 0.0)\nupdate!(fixed_elements, \"displacement 3\", 0.0)Let\'s use simple acceleration load.update!(model_elements, \"displacement load 1\", 1.0)Finally the ´Analysis` couples everything togeter.analysis = Analysis(Linear, model, fixed)Let\'s write resuls to Xdmf filexdmf = Xdmf(\"model_results\"; overwrite=true)\nadd_results_writer!(analysis, xdmf)Now we have all we need to run the analysis.run!(analysis)"
+},
+
+{
+    "location": "examples/linear_static.html#Postprocessing-1",
+    "page": "JuliaFEM Linear Static Example",
+    "title": "Postprocessing",
+    "category": "section",
+    "text": "This is how the stresses are requestedpush!(model.postprocess_fields, \"stress\")Finally let\'s write the results to the xdmf (this is bug, the solver should do this automatically, see issue #203)time = 0.0\nJuliaFEM.postprocess!(analysis, time)\nJuliaFEM.write_results!(analysis, time)In order to look the results, we will need to close the xdmf that it is actually written to the file from buffer.close(xdmf.hdf)Finally when we open the model in ParaView and set some settings we have this end result.(Image: )"
+},
+
+{
+    "location": "examples/linear_static.html#Testing-1",
+    "page": "JuliaFEM Linear Static Example",
+    "title": "Testing",
+    "category": "section",
+    "text": "First let\'s test that we have the output files writen to the diskif VERSION < v\"1.0.0\"\n    using Base.Test\nelse\n    using Test\nend\n\n@test isfile(\"model_results.xmf\")\n@test isfile(\"model_results.h5\")Secondly let\'s test that we have the same maximum displacement each time. This is also an usefull example how to access the displacements values.time = 0.0\nu = analysis(\"displacement\", time)\nu_norms = Dict(i => norm(j) for (i, j) in u)\n@test isapprox(maximum(values(u_norms)),2.4052929896922337)This page was generated using Literate.jl."
+},
+
+{
     "location": "packages/FEMBase/mesh.html#",
     "page": "Mesh",
     "title": "Mesh",
